bc61ef19588981d89762980d89f2b3e718a9652a
UmbertoB <umbertobarrosf@gmail.com>
2021-06-08 23:22:43 -0300

    db cred

--- packages/prodoc-backend/src/database/index.js
+++ packages/prodoc-backend/src/database/index.js
@@ -8,7 +8,7 @@ module.exports = require('knex')({
     connection: {
         user: 'fcthwmjxcyqoca',
         password: '909e3eea762710894e28735dd5c0a89d0220cfd2782e139b3832d49359f1506c',
-        server: 'ec2-54-224-194-214.compute-1.amazonaws.com',
+        host: 'ec2-54-224-194-214.compute-1.amazonaws.com',
         port: 5432,
         database: 'd41bhlj4cl0u5p'
     },
